39bf285b678d31a1ab9e0dc58730f3bf1bc39b76
DevIsraelHub <devisrael01@gmail.com>
2023-09-10 15:01:40 +0100

    last commit

--- vite-port/src/components/portfolio/Items.jsx
+++ vite-port/src/components/portfolio/Items.jsx
@@ -6,15 +6,18 @@ import { motion } from 'framer-motion';
 import Project2 from '../../assets/travel.png';
 import Project3 from '../../assets/port-img.png';
 import Project5 from '../../assets/fastify.png';
+import Project4 from '../../assets/glowing.png';
 
 import ProjectModal from '../Modals/ProjectModal';
 import ProjectModalThree from '../Modals/ProjectModalThree';
 import ProjectModaltwo from '../Modals/ProjectModalTwo';
+import ProjectModalFour from '../Modals/ProjectModalFour';
 
 const Items = () => {
   const [isOpen, setIsOpen] = useState(false)
   const [isOpenTwo, setIsOpenTwo] = useState(false)
   const [isOpenThree, setIsOpenThree] = useState(false)
+  const [isOpenFour, setIsOpenFour] = useState(false)
 
 
   return (
@@ -51,6 +54,22 @@ const Items = () => {
               </button>
             </motion.div>
             <ProjectModaltwo img={Project2} open={isOpenTwo} onClose={() => setIsOpenTwo(false)} />
+            <motion.div
+              layout
+              animate={{ opacity: 1, scale: 1 }}
+              initial={{ opacity: 0.8, scale: 0.6 }}
+              exit={{ opacity: 0.8, scale: 0.6 }}
+              transition={{ duration: 0.3 }}
+              className="portfolio__items card card-two">
+              <div className="portfolio__img-wrapper">
+                <img onClick={() => setIsOpenFour(true)} src={Project4} alt="project  image" className='portfolio__img' />
+              </div>
+              <button onClick={() => setIsOpenFour(true)} className='link'>
+                View more
+                <FaArrowRight className='link__icon'></FaArrowRight>
+              </button>
+            </motion.div>
+              <ProjectModalFour img={Project4} open={isOpenFour} onClose={() => setIsOpenFour(false)} />
             <motion.div
               layout
               animate={{ opacity: 1, scale: 1 }}
