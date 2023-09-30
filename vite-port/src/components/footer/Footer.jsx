7b1a7379c142a57de7f3cabe904092e694c32603
DevIsraelHub <devisrael01@gmail.com>
2023-10-01 00:16:36 +0100

    commit message

--- vite-port/src/components/footer/Footer.jsx
+++ vite-port/src/components/footer/Footer.jsx
@@ -1,29 +1,26 @@
 import React from 'react'
 import './footer.css'
-import {  FaInstagram, FaLinkedin, FaTwitter } from 'react-icons/fa'
+import { FaLinkedin } from 'react-icons/fa'
 
 const Footer = () => {
-  return (
-    <footer className='footer'>
-      <div className="footer__container container grid">
-        <div className="footer__socials">
-            <a target='blank' title='View twitter profile'  href="https://twitter.com/Dev__Israel/" className="footer__social-link">
-                <FaTwitter />
-            </a>
-            <a target='blank' title='View instagram profile' href="https://www.instagram.com/dev__israel/" className="footer__social-link">
-                <FaInstagram />
-            </a>
-        </div>
+   return (
+      <footer className='footer'>
+         <div className="footer__container container grid">
+            <div className="footer__socials">
+               <a title='View LinkedIn profile' href="https://linkedin.com/in/dev--israel/" className="home__social-link">
+                  <FaLinkedin />
+               </a>
+            </div>
 
-        <p className="footer__copyright text-cs">
-          &copy; Copyright 2023. All Rights Reserved
-        </p>
-        <p className="footer__copyright text-cs">
-          Developed By <span title='Dev Israel'>Dev Israel</span>
-        </p>
-      </div>
-    </footer>
-  )
+            <p className="footer__copyright text-cs">
+               &copy; Copyright 2023. All Rights Reserved
+            </p>
+            <p className="footer__copyright text-cs">
+               Developed By <span title='Dev Israel'>Dev Israel</span>
+            </p>
+         </div>
+      </footer>
+   )
 }
 
 export default Footer
--- vite-port/src/components/header/Header.jsx
+++ vite-port/src/components/header/Header.jsx
@@ -7,116 +7,108 @@ import { Link } from 'react-scroll'
 import { animateScroll } from 'react-scroll'
 
 
-// const getStorageTheme = () => {
-//   let theme = 'dark-theme'
-//   if (localStorage.getItem('theme'))
-//   {
-//     theme = localStorage.getItem('theme')
-//   }
-// }
-
 const Header = () => {
 
-  const [showMenu, setShowMenu] = useState(false)
-  const [scrollNav, setScrollNav] = useState(false)
-  const [theme, setTheme] = useState(true)
+   const [showMenu, setShowMenu] = useState(false)
+   const [scrollNav, setScrollNav] = useState(false)
+   const [theme, setTheme] = useState(true)
 
-  const scrollTop = () => {
-    animateScroll.scrollToTop()
-  }
+   const scrollTop = () => {
+      animateScroll.scrollToTop()
+   }
 
-  const changeNav = () => {
-    if (window.scrollY >= 80)
-    {
-      setScrollNav(true)
-    } else
-    {
-      setScrollNav(false)
-    }
-  }
+   const changeNav = () => {
+      if (window.scrollY >= 80)
+      {
+         setScrollNav(true)
+      } else
+      {
+         setScrollNav(false)
+      }
+   }
 
-  const toggleTheme = () => {
-    setTheme(prev => !prev)
-  }
-  // localStorage.clear()
-   
-  
-  useEffect(() => {
-    localStorage.setItem('lightTheme', JSON.stringify(theme))
-  }, [theme])
+   const toggleTheme = () => {
+      setTheme(prev => !prev)
+   }
+   // localStorage.clear()
 
-  useEffect(() => {
-    window.addEventListener('scroll', changeNav)
-  }, [])
 
-  useEffect(() => {
-    document.body.classList.toggle('no-scroll', showMenu)
-  }, [showMenu])
+   useEffect(() => {
+      localStorage.setItem('lightTheme', JSON.stringify(theme))
+   }, [theme])
 
-  useEffect(() => {
-    document.documentElement.className = theme ? 'lightTheme' : 'darkTheme'
-  }, [theme])
-  useEffect(() => {
-    const newTheme = JSON.parse(localStorage.getItem('lightTheme'))
-    setTheme(newTheme)
-  }, [])
+   useEffect(() => {
+      window.addEventListener('scroll', changeNav)
+   }, [])
 
-  return (
-    <header className={`${scrollNav ? 'scroll-header' : ''} header`}>
-      <div onClick={() => setShowMenu(false)}  className={`${showMenu ? 'overlay-menu': ''} overlay`}></div>
-      <nav className="nav">
-        <a 
-          title='Go to Dev home'
-          href='#home'
-          className="nav__logo text-cs"
-          onClick={scrollTop}>
-          Dev.</a>
+   useEffect(() => {
+      document.body.classList.toggle('no-scroll', showMenu)
+   }, [showMenu])
 
-        <div className={
-          `${showMenu ? 'nav__menu show-menu' : 'nav__menu'}`
-        }>
-          <div className="nav__data">
-            <ul className="nav__list">
-              {links.map(({ name, path }, index) => {
-                return (
-                  <li className="nav__item" key={index}>
-                    <Link
-                      title={`Go to ${name}`}
-                      className='nav__link text-cs'
-                      to={path}
-                      spy={true}
-                      smooth={true}
-                      hashSpy={true}
-                      offset={-150}
-                      duration={100}
-                      onClick={() => setShowMenu(false)}
-                    >
-                      {name}
-                    </Link>
-                  </li>
-                )
-              })}
-            </ul>
-          </div>
-          <div className="section__deco deco__left header__deco">
-            <img src={shapeOne} alt="decoration design" className="shape" />
-          </div>
-        </div>
-        <div className="nav__btns">
-          <div title='Toggle Theme' className="theme__toggler" onClick={toggleTheme}>
-            {theme == 'light-theme' ? <BsMoon /> : <BsSun />}
-          </div>
-          <div title='menu bar' className={
-            `${showMenu ? 'nav__toggle animate-toggle' : 'nav__toggle'}`
-          }
-            onClick={() => setShowMenu(prev => !prev)}>
-            <span></span>
-            <span></span>
-          </div>
-        </div>
-      </nav>
-    </header>
-  )
+   useEffect(() => {
+      document.documentElement.className = theme ? 'lightTheme' : 'darkTheme'
+   }, [theme])
+   useEffect(() => {
+      const newTheme = JSON.parse(localStorage.getItem('lightTheme'))
+      setTheme(newTheme)
+   }, [])
+
+   return (
+      <header className={`${scrollNav ? 'scroll-header' : ''} header`}>
+         <div onClick={() => setShowMenu(false)} className={`${showMenu ? 'overlay-menu' : ''} overlay`}></div>
+         <nav className="nav">
+            <a
+               title='Go to Dev home'
+               href='#home'
+               className="nav__logo text-cs"
+               onClick={scrollTop}>
+               Dev.</a>
+
+            <div className={
+               `${showMenu ? 'nav__menu show-menu' : 'nav__menu'}`
+            }>
+               <div className="nav__data">
+                  <ul className="nav__list">
+                     {links.map(({ name, path }, index) => {
+                        return (
+                           <li className="nav__item" key={index}>
+                              <Link
+                                 title={`Go to ${name}`}
+                                 className='nav__link text-cs'
+                                 to={path}
+                                 spy={true}
+                                 smooth={true}
+                                 // hashSpy={true}
+                                 offset={-150}
+                                 duration={100}
+                                 onClick={() => setShowMenu(false)}
+                              >
+                                 {name}
+                              </Link>
+                           </li>
+                        )
+                     })}
+                  </ul>
+               </div>
+               <div className="section__deco deco__left header__deco">
+                  <img src={shapeOne} alt="decoration design" className="shape" />
+               </div>
+            </div>
+            <div className="nav__btns">
+               <div title='Toggle Theme' className="theme__toggler" onClick={toggleTheme}>
+                  {theme == 'light-theme' ? <BsMoon /> : <BsSun />}
+               </div>
+               <div title='menu bar' className={
+                  `${showMenu ? 'nav__toggle animate-toggle' : 'nav__toggle'}`
+               }
+                  onClick={() => setShowMenu(prev => !prev)}>
+                  <span></span>
+                  <span></span>
+               </div>
+            </div>
+         </nav>
+      </header>
+   )
 }
 
 export default Header
